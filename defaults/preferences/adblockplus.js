28b1bb5c8b9609402e1c93163ca5da6688a241e8
Wladimir Palant <trev@gtchat.de>
2006-01-17 14:05:08 +0000

    Committing extension sources

---
+++ defaults/preferences/adblockplus.js
@@ -0,0 +1,9 @@
+pref("extensions.adblockplus.enabled", true);
+pref("extensions.adblockplus.frameobjects", true);
+pref("extensions.adblockplus.fastcollapse", false);
+pref("extensions.adblockplus.linkcheck", true);
+pref("extensions.adblockplus.listsort", true);
+pref("extensions.adblockplus.warnregexp", true);
+pref("extensions.adblockplus.checkedadblockprefs", false);
+
+pref("extensions.adblockplus.patterns", "*/banner/* */banners/* http://*.adserver.example.com/*");
---
+++ install.js
@@ -0,0 +1,81 @@
+// constants
+const APP_DISPLAY_NAME = "Adblock Plus";
+const APP_NAME = "adblockplus";
+const APP_PACKAGE = "/adblockplus.mozdev.org";
+const APP_VERSION = "{{VERSION}}";
+const WARNING = "WARNING: You need administrator priviledges to install Adblock Plus. It will be installed in the application directory for all users. Installing Adblock Plus in your profile is only supported for Firefox 0.9+. Proceed with the installation?";
+
+if (confirm(WARNING)) {
+  /* Pre-Install Cleanup (for prior versions) */
+  
+  // file-check array
+  var dirArray = [
+    [getFolder("Profile", "chrome"), "adblockplus.jar"],      // Profile jar
+    [getFolder("Chrome"), "adblockplus.jar"],                 // Root jar
+    [getFolder("Components"), "nsAdblockPlus.js"],            // Root component
+    [getFolder("Profile", "components"), "nsAdblockPlus.js"], // Profile component
+    [getFolder("Profile"), "XUL FastLoad File"],              // XUL cache Mac Classic
+    [getFolder("Profile"), "XUL.mfast"],                      // XUL cache MacOS X
+    [getFolder("Profile"), "XUL.mfasl"],                      // XUL cache Linux
+    [getFolder("Profile"), "XUL.mfl"]                         // XUL cache Windows
+  ];
+  
+  // Remove any existing files
+  initInstall("pre-install", "/rename", "0.0");  // open dummy-install
+  for (var i = 0 ; i < dirArray.length ; i++) {
+    var currentDir = dirArray[i][0];
+    var name = dirArray[i][1];
+    var oldFile = getFolder(currentDir, name);
+
+    // Find a name to rename the file into
+    var newName = name + "-uninstalled";
+    for (var n = 1; File.exists(oldFile) && File.exists(getFolder(currentDir, newName)); n++)
+      newName = name + n + "-uninstalled";
+  
+    if (File.exists(oldFile))
+      File.rename(oldFile, newName);
+  }
+  performInstall(); // commit renamed files
+
+  /* Main part of the installation */
+
+  var files = [
+    ["chrome/adblockplus.jar", getFolder("Chrome")],
+    ["components/nsAdblockPlus.js", getFolder("Components")],
+    ["defaults/preferences/adblockplus.js", getFolder(getFolder("Program", "defaults"), "pref")],
+  ];
+  
+  // initialize our install
+  initInstall(APP_NAME, APP_PACKAGE, APP_VERSION);
+  
+  // Add files
+  for (var i = 0; i < files.length; i++)
+    addFile(APP_NAME, APP_VERSION, files[i][0], files[i][1], null);
+
+  var jar = getFolder(getFolder("Chrome"), "adblockplus.jar");
+  try {
+    var err = registerChrome(CONTENT | DELAYED_CHROME, jar, "content/");
+    if (err != SUCCESS)
+      throw "Chrome registration for content failed (error code " + err + ").";
+
+    err = registerChrome(SKIN | DELAYED_CHROME, jar, "skin/classic/");
+    if (err != SUCCESS)
+      throw "Chrome registration for skin failed (error code " + err + ").";
+
+    err = registerChrome(LOCALE | DELAYED_CHROME, jar, "locale/en-US/");
+    if (err != SUCCESS)
+      throw "Chrome registration for en-US locale failed (error code " + err + ").";
+
+    var err = performInstall();
+    if (err != SUCCESS && err != 999)
+      throw "Committing installation failed (error code " + err + ").";
+
+    alert("Adblock Plus " + APP_VERSION + " is now installed.\n" +
+          "It will become active after you restart your browser.");
+  }
+  catch (ex) {
+    alert("Installation failed: " + ex + "\n" +
+          "You probably don't have the necessary permissions (log in as system administrator).");
+    cancelInstall(err);
+  } 
+}
