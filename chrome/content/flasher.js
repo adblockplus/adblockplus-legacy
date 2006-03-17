18b86e58b68cfc370bca1731da76aa16399329e0
Wladimir Palant <trev@gtchat.de>
2006-03-17 21:27:33 +0000

    Scroll to item when flashing

--- chrome/content/flasher.js
+++ chrome/content/flasher.js
@@ -32,10 +32,84 @@ var flasher = {
   count: 0,
   timer: null,
 
+  getCoords: function(insecNode) {
+    var insecBox = secureLookup(insecNode, "ownerDocument", "getBoxObjectFor")(insecNode);
+    if (!insecBox)
+      return null;
+
+    var ret = [0, 0, secureGet(insecBox, "screenX"), secureGet(insecBox, "screenY")];
+    while (insecBox) {
+      ret[0] += secureGet(insecBox, "x");
+      ret[1] += secureGet(insecBox, "y");
+      insecBox = secureGet(insecBox, "parentBox");
+    }
+
+    return ret;
+  },
+
+  visibleCoords: function(insecWnd, coords) {
+    var minX = secureGet(insecWnd, "scrollX");
+    var maxX = minX + secureGet(insecWnd, "innerWidth");
+    var minY = secureGet(insecWnd, "scrollY");
+    var maxY = minY + secureGet(insecWnd, "innerHeight");
+
+    return (coords[0] >= minX && coords[0] < maxX &&
+            coords[1] >= minY && coords[1] < maxY);
+  },
+
   flash: function(inseclNodes) {
     this.stop();
     if (!inseclNodes)
       return;
+
+    inseclNodes = inseclNodes.slice();
+    if (inseclNodes.length) {
+      // Ensure that at least one node is visible when flashing
+      var minCoords = null;
+      var insecShowWnd = null;
+      for (var i = 0; i < inseclNodes.length; i++) {
+        var insecContentBody = secureGet(inseclNodes[i], "document", "body");
+        if (insecContentBody)
+          inseclNodes[i] = insecContentBody;   // for frames
+
+        var coords = this.getCoords(inseclNodes[i]);
+        if (!coords)
+          continue;
+
+        var insecWnd = secureGet(inseclNodes[0], "ownerDocument", "defaultView");
+        if (!insecWnd)
+          continue;
+  
+        // Check whether node's top left corner is already visible
+        if (this.visibleCoords(insecWnd, coords)) {
+          minCoords = null;
+          insecShowWnd = insecWnd;
+          break;
+        }
+
+        // Check whether this node's coordinates are smaller than the ones we found
+        if (!minCoords || coords[1] < minCoords[1] || (coords[1] == minCoords[1] && coords[0] < minCoords[0])) {
+          minCoords = coords;
+          insecShowWnd = insecWnd;
+        }
+      }
+
+      if (minCoords)
+        secureLookup(insecShowWnd, "scrollTo")(minCoords[0], minCoords[1]);
+
+      if (insecShowWnd) {
+        while (insecShowWnd) {
+          var insecParentNode = secureGet(insecShowWnd, "frameElement");
+          var insecShowWnd = secureGet(insecShowWnd, "parent");
+          if (!insecParentNode || !insecShowWnd)
+            break;
+
+          coords = this.getCoords(insecParentNode);
+          if (coords && !this.visibleCoords(insecShowWnd, coords))
+            secureLookup(insecShowWnd, "scrollTo")(coords[0], coords[1]);
+        }
+      }
+    }
 
     this.inseclNodes = inseclNodes;
     this.count = 0;
@@ -72,14 +146,8 @@ var flasher = {
   },
 
   setOutline: function(value) {
-    for (var i = 0; i < this.inseclNodes.length; i++) {
-      var insecNode = this.inseclNodes[i];
-      var insecContentBody = secureGet(insecNode, "document", "body");
-      if (insecContentBody)
-        insecNode = insecContentBody;   // for frames
-
-      secureSet(insecNode, "style", "MozOutline", value);
-    }
+    for (var i = 0; i < this.inseclNodes.length; i++)
+      secureSet(this.inseclNodes[i], "style", "MozOutline", value);
   },
 
   switchOn: function() {
