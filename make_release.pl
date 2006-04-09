8ebdf0639b25d4fb96c9dcc3bbd57553aeae1796
Wladimir Palant <trev@gtchat.de>
2006-04-09 00:59:50 +0000

    Execute only one CVS call to add and remove the files
    --HG--
    extra : convert_revision : svn%3Ad8bf93c1-8190-44a8-bb31-1ea94378a4df/trunk%40286

--- make_release.pl
+++ make_release.pl
@@ -4,7 +4,8 @@ use strict;
 
 die "Version number not specified" unless @ARGV;
 
-my @cvs = ();
+my @remove = ();
+my @add = ();
 
 my $version = $ARGV[0];
 $version =~ s/[^\w\.]//gs;
@@ -15,7 +16,7 @@ close(VERSION);
 
 @ARGV = ("../downloads/adblockplus-$version.xpi");
 do 'create_xpi.pl';
-push @cvs, qq(add downloads/adblockplus-$version.xpi);
+push @add, qq(downloads/adblockplus-$version.xpi);
 
 opendir(LOCALES, "chrome/locale");
 my @locales = grep {!/[^\w\-]/ && !/CVS/} readdir(LOCALES);
@@ -27,7 +28,7 @@ map {
   if (tr/-// > 1)
   {
     unlink("../downloads/$_");
-    push @cvs, qq(remove downloads/$_);
+    push @remove, qq(downloads/$_);
   }
 } readdir(DOWNLOADS);
 closedir(DOWNLOADS);
@@ -37,11 +38,10 @@ for my $locale (@locales)
 {
   @ARGV = ("../downloads/adblockplus-$version-$locale.xpi", $locale);
   do 'create_xpi.pl';
-  push @cvs, qq(add downloads/adblockplus-$version-$locale.xpi);
+  push @add, qq(downloads/adblockplus-$version-$locale.xpi);
 }
 
-push @cvs, qq(commit -m "Releasing Adblock Plus $version");
-
 chdir('..');
-system("cvs $_") foreach @cvs;
-
+system("cvs remove @remove") if @remove;
+system("cvs add @add") if @add;
+system(qq(cvs commit -m "Releasing Adblock Plus $version"));
