2e3f8664eee7c1f96633f8689020f4ef148694c4
Wladimir Palant <trev@gtchat.de>
2006-08-12 22:27:34 +0000

    Check whether we have write permissions for all target directories

--- install.js
+++ install.js
@@ -3,8 +3,9 @@ const APP_DISPLAY_NAME = "Adblock Plus";
 const APP_NAME = "adblockplus";
 const APP_PACKAGE = "/adblockplus.mozdev.org";
 const APP_VERSION = "{{VERSION}}";
-const WARNING = "WARNING: You need administrator priviledges to install Adblock Plus. It will be installed in the application directory for all users. Installing Adblock Plus in your profile is currently not supported in SeaMonkey. Proceed with the installation?";
+const WARNING = "WARNING: You need administrator privileges to install Adblock Plus. It will be installed in the application directory for all users. Installing Adblock Plus in your profile is currently not supported in SeaMonkey. Proceed with the installation?";
 const VERSION_ERROR = "This extension can only be installed in a browser based on Gecko 1.8 or higher, please upgrade your browser. Compatible browsers include Firefox 1.5, SeaMonkey 1.0 and Flock 0.5.";
+const NOT_WRITABLE_ERROR = "This extension requires write access to the application directory to install properly. Currently write access to some of the relevant subdirectories is forbidden, you probably have to log in as root before installing. After installation no elevated privileges will be necessary, read access is sufficient to use Adblock Plus."
 const locales = [
   "{{LOCALE}}",
   null
@@ -15,11 +16,28 @@ var incompatible = (typeof Install.BUTTON_POS_0 == "undefined");
 if (incompatible)
   alert(VERSION_ERROR);
 
+if (!incompatible) {
+  // Check whether all directories can be accessed
+  var profileInstall = new String(Install.url).match(/profile[^\/]*$/);
+  var jarFolder = (profileInstall ? getFolder("Profile", "chrome") : getFolder("Chrome"));
+  var dirList = [
+    jarFolder,
+    getFolder("Components"),
+    getFolder(getFolder("Program", "defaults"), "pref")
+  ];
+  for (var i = 0; i < dirList.length; i++)
+    if (!File.isWritable(dirList[i]))
+      incompatible = true;
+
+  if (incompatible)
+    alert(NOT_WRITABLE_ERROR);
+}
+
 if (!incompatible && confirm(WARNING, APP_DISPLAY_NAME)) {
   /* Pre-Install Cleanup (for prior versions) */
   
-  // file-check array
-  var dirArray = [
+  // List of files to be checked
+  var checkFiles = [
     [getFolder("Profile", "chrome"), "adblockplus.jar"],      // Profile jar
     [getFolder("Chrome"), "adblockplus.jar"],                 // Root jar
     [getFolder("Components"), "nsAdblockPlus.js"],            // Root component
@@ -30,12 +48,12 @@ if (!incompatible && confirm(WARNING, APP_DISPLAY_NAME)) {
     [getFolder("Profile"), "XUL.mfasl"],                      // XUL cache Linux
     [getFolder("Profile"), "XUL.mfl"]                         // XUL cache Windows
   ];
-  
+
   // Remove any existing files
   initInstall("pre-install", "/rename", "0.0");  // open dummy-install
-  for (var i = 0 ; i < dirArray.length ; i++) {
-    var currentDir = dirArray[i][0];
-    var name = dirArray[i][1];
+  for (var i = 0 ; i < checkFiles.length ; i++) {
+    var currentDir = checkFiles[i][0];
+    var name = checkFiles[i][1];
     var oldFile = getFolder(currentDir, name);
 
     // Find a name to rename the file into
@@ -50,8 +68,6 @@ if (!incompatible && confirm(WARNING, APP_DISPLAY_NAME)) {
 
   /* Main part of the installation */
 
-  var profileInstall = new String(Install.url).match(/profile[^\/]*$/);
-  var jarFolder = (profileInstall ? getFolder("Profile", "chrome") : getFolder("Chrome"));
   var chromeType = (profileInstall ? PROFILE_CHROME : DELAYED_CHROME);
 
   var files = [
