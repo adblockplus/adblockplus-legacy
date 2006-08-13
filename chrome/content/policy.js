53ca764ba92ed6c186c90c68b5cde4f93bbbefdc
Wladimir Palant <trev@gtchat.de>
2006-08-13 19:46:22 +0000

    Fixing bustage from bug 14793, frames wouldn't be blocked
    --HG--
    extra : convert_revision : svn%3Ad8bf93c1-8190-44a8-bb31-1ea94378a4df/trunk%40405

--- chrome/content/policy.js
+++ chrome/content/policy.js
@@ -83,7 +83,7 @@ var policy = {
     if (!topWnd || !topWnd.location || !topWnd.location.href)
       return true;
 
-    var wndLocation = unwrapURL(wnd.location.href);
+    var wndLocation = (contentType == type.SUBDOCUMENT ? location : unwrapURL(wnd.location.href));
     var topLocation = unwrapURL(topWnd.location.href);
     var blockable = this.isBlockableScheme(wndLocation);
     if (!blockable && prefs.blocklocalpages && this.isLocalScheme(wndLocation))
