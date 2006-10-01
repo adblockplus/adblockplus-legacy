a1c9f2b8ebf25e690ba2cb81faab1666b2cd7c51
Wladimir Palant <trev@gtchat.de>
2006-10-01 17:14:58 +0000

    Allow creating "debug" builds

--- create_xpi.pl
+++ create_xpi.pl
@@ -15,6 +15,36 @@ if ($ARGV[0] =~ /^\+/)
   shift @ARGV;
 }
 
+my $debug = ($version =~ /debug$/);
+my $abpPostfix = "";
+if ($debug) {
+  $abpPostfix = <<'EOT';
+    var logFile = Components.classes["@mozilla.org/file/directory_service;1"]
+                            .getService(Components.interfaces.nsIProperties)
+                            .get("DrvD", Components.interfaces.nsILocalFile);
+    logFile.append("adblockplus.log");
+
+    var logFileOutput = Components.classes["@mozilla.org/network/file-output-stream;1"]
+                                  .createInstance(Components.interfaces.nsIFileOutputStream);
+    logFileOutput.init(logFile, 0x20 | 0x08 | 0x02, 0644, 0);
+
+    abp.log = function abpLog(file, line, funcName, args, action) {
+      args = Array.prototype.join.call(args, ", ");
+      if (args.length > 100)
+        args = args.substr(0, 100) + "...";
+      var signature = funcName + "(" + args + ")";
+      var numFilters = "";
+      if (abp && "prefs" in abp && abp.prefs)
+        numFilters = " - " + abp.prefs.userPatterns.length + " filters in the list";
+      var str = file + " line " + line + ", " + action + " in " + signature + numFilters + "\n";
+    
+      logFileOutput.write(str, str.length);
+    };
+EOT
+  $abpPostfix =~ s/^\s+//mg;
+  $abpPostfix =~ s/[\r\n]//sg;
+}
+
 my @locales;
 if (@ARGV)
 {
@@ -76,6 +106,13 @@ sub cp
 {
   my ($fromfile, $tofile, $replace_version) = @_;
 
+  my $funcName = '';
+  my $fileName = $tofile;
+  $fileName =~ s/.*\///;
+  my $lineNum = 1;
+  my $retNum;
+  my $throwNum;
+
   my $text = ($fromfile =~ /\.(manifest|xul|js|xml|xhtml|rdf|dtd|properties|css)$/);
   open(local *FROM, $fromfile) or return;
   open(local *TO, ">$tofile") or return;
@@ -97,6 +134,26 @@ sub cp
         }
         $_ = $loc;
       }
+      if ($debug && $tofile =~ /\.js/) {
+        if (/function (\w+)\([^)]*\)\s*\{\s*/ || /(\w+)\s*[:=]\s*function\([^)]*\)\s*\{\s*/ || /([gs]et\s+\w+)\([^)]*\)\s*\{\s*/) {
+          $funcName = $1;
+          $retNum = 1;
+          $throwNum = 1;
+          my $call = "if (abp) abp.log('$fileName', $lineNum, '$funcName', arguments, 'enter');";
+          s/(function \w+\([^)]*\)\s*\{[^\S\n]*)/$1$call/
+            || s/(\w+\s*[:=]\s*function\([^)]*\)\s*\{[^\S\n]*)/$1$call/
+            || s/([gs]et\s+\w+\([^)]*\)\s*\{[^\S\n]*)/$1$call/;
+        }
+        if ($funcName && /return[^;]*;/) {
+          s/(return[^;]*;)/{if (abp) abp.log('$fileName', $lineNum, '$funcName', arguments, 'return$retNum');$1}/;
+          $retNum++;
+        }
+        if ($funcName && /throw[^;]*;/) {
+          s/(throw[^;]*;)/{if (abp) abp.log('$fileName', $lineNum, '$funcName', arguments, 'throw$throwNum');$1}/;
+          $throwNum++;
+        }
+      }
+      $lineNum += tr/\n//;
       $_;
     } <FROM>;
   }
@@ -106,6 +163,9 @@ sub cp
     binmode(FROM) unless $text;
     print TO <FROM>;
   }
+
+  print TO $abpPostfix if $debug && $tofile =~ /nsAdblockPlus.js$/;
+
   close(TO);
   close(FROM);
 }
