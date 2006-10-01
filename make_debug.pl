4f450cca4e7a70e3bde80f23531fe9b210ffc389
Wladimir Palant <trev@gtchat.de>
2006-10-01 17:14:58 +0000

    Allow creating "debug" builds
    --HG--
    extra : convert_revision : svn%3Ad8bf93c1-8190-44a8-bb31-1ea94378a4df/trunk%40491

---
+++ make_debug.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+
+open(VERSION, "version");
+my $version = <VERSION>;
+$version =~ s/[^\w\.]//gs;
+close(VERSION);
+
+my ($sec, $min, $hour, $day, $mon, $year) = localtime;
+my $build = sprintf("%04i%02i%02i%02i", $year+1900, $mon+1, $day, $hour);
+
+my $locale = (@ARGV ? "-" . join("-", @ARGV) : "");
+@ARGV = ("adblockplus-$version+.${build}debug$locale.xpi", "+.${build}debug", @ARGV);
+do 'create_xpi.pl';
