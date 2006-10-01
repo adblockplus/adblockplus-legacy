b5e180e98d51a209724e82dce828f8f0b6843733
Wladimir Palant <trev@gtchat.de>
2006-10-01 15:11:13 +0000

    Fixing comment
    --HG--
    extra : convert_revision : svn%3Ad8bf93c1-8190-44a8-bb31-1ea94378a4df/trunk%40488

--- chrome/content/prefs.js
+++ chrome/content/prefs.js
@@ -565,7 +565,7 @@ var prefs = {
 
   getFileByPath: function(path) {
     try {
-      // Assume a relative path first
+      // Assume an absolute path first
       var file = Components.classes["@mozilla.org/file/local;1"]
                            .createInstance(Components.interfaces.nsILocalFile);
       file.initWithPath(path);
