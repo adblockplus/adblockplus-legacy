8ac2aa81c8a72941b3998de5c6bc73642ab9502c
Wladimir Palant <trev@gtchat.de>
2006-10-28 07:37:37 +0000

    Add languages

--- kmeleon/build_all.pl
+++ kmeleon/build_all.pl
@@ -5,3 +5,5 @@ use strict;
 system("$^X build.pl adblockplus-kmeleon-en-US.zip en-US");
 system("$^X build.pl adblockplus-kmeleon-de-DE.zip de-DE");
 system("$^X build.pl adblockplus-kmeleon-ru-RU.zip ru-RU");
+system("$^X build.pl adblockplus-kmeleon-fr-FR.zip fr-FR");
+system("$^X build.pl adblockplus-kmeleon-es-ES.zip es-ES");
