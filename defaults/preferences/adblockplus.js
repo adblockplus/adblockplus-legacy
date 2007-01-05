c314c2f9274ad1978adeac14387a59c6a14b27c0
Wladimir Palant <trev@gtchat.de>
2007-01-05 14:19:26 +0000

    Block remote images in data: and javascript: frames

--- defaults/preferences/adblockplus.js
+++ defaults/preferences/adblockplus.js
@@ -31,7 +31,7 @@ pref("extensions.adblockplus.blocktypes", "script image stylesheet object subdoc
 pref("extensions.adblockplus.linktypes", "image object");
 pref("extensions.adblockplus.noncollapsabletypes", "script stylesheet background");
 pref("extensions.adblockplus.whitelistschemes", "about chrome file irc mailto news resource snews x-jsd addbook cid imap mailbox nntp pop data javascript");
-pref("extensions.adblockplus.localschemes", "file imap mailbox nntp news pop");
+pref("extensions.adblockplus.localschemes", "file imap mailbox nntp news pop data javascript");
 pref("extensions.adblockplus.hideimagemanager", true);
 pref("extensions.adblockplus.protectchrome.abp", "adblockplus");
 pref("extensions.{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}.description", "chrome://adblockplus/locale/global.properties");
