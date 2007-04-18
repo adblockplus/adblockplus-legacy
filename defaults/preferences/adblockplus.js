09195ac2816d308cdddd1d333db17452109fb88e
Wladimir Palant <trev@gtchat.de>
2007-04-18 01:37:09 +0000

    Call fallback after more errors, don't want to be flooded with requests
    --HG--
    extra : convert_revision : svn%3Ad8bf93c1-8190-44a8-bb31-1ea94378a4df/trunk%40746

--- defaults/preferences/adblockplus.js
+++ defaults/preferences/adblockplus.js
@@ -26,5 +26,5 @@ pref("extensions.adblockplus.whitelistschemes", "about chrome file irc mailto ne
 pref("extensions.adblockplus.hideimagemanager", true);
 pref("extensions.adblockplus.protectchrome.abp", "adblockplus");
 pref("extensions.adblockplus.subscriptions_fallbackurl", "http://adblockplus.org/getSubscription?url=%s");
-pref("extensions.adblockplus.subscriptions_fallbackerrors", 20);
+pref("extensions.adblockplus.subscriptions_fallbackerrors", 30);
 pref("extensions.{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}.description", "chrome://adblockplus/locale/global.properties");
