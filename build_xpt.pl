8b88b60ff03c2a3a58254bbb417ca19dcb6064f6
Wladimir Palant <trev@gtchat.de>
2007-09-14 10:25:56 +0000

    Remove nsIAdblockPlus interface to avoid detection

--- build_xpt.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-
-use strict;
-
-my $GECKO_DIR = 'c:\FFTree1\mozilla\dist';
-system(qq($GECKO_DIR\\bin\\xpidl.exe -m typelib -I $GECKO_DIR\\idl -e components\\nsAdblockPlus.xpt nsAdblockPlus.idl));
