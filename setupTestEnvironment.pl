0c273441193dba6476418dfb5284b9b9bd8a7c84
trev <none@none>
2008-10-22 21:00:06 +0200

    Generalize script to set up test environment and add it to EHH and ABP Watcher extensions as well

--- setupTestEnvironment.pl
+++ setupTestEnvironment.pl
@@ -4,28 +4,6 @@ use strict;
 use warnings;
 use Cwd;
 
-my @dirs;
-if (open(local *DIRS, ".profileDirs"))
-{
-  @dirs = map {s/[\r\n]//g;$_} <DIRS>;
-  close(DIRS);
-}
-my (@syncDirs, $pkg);
-
-unless (@dirs)
-{
-  print STDERR <<'EOT';
-This script requires a file with the name .profileDirs to be in the current
-directory. Please create this file and specify the directories of the profiles
-where you want to install a test version of Adblock Plus, one per line. For
-example:
-
-  c:\Documents and Setting\<user>\Application Data\Mozilla\Firefox\Profiles\<seed>.default
-  c:\Documents and Setting\<user>\Application Data\Songbird1\Profiles\<seed>.default
-EOT
-  exit 1;
-}
-
 my $version = readFile("version");
 $version =~ s/[^\w\.]//gs;
 
@@ -34,16 +12,53 @@ my @locales = grep {!/[^\w\-]/} readdir(LOCALES);
 @locales = sort {$a eq "en-US" ? -1 : ($b eq "en-US" ? 1 : $a cmp $b)} @locales; 
 closedir(LOCALES);
 
-my $install = fixupFile(readFile("install.rdf"));
-my $manifest = fixupFile(readFile("chrome.manifest"));
-my $component = fixupFile(readFile("components/nsAdblockPlus.js"));
-my $prefs = fixupFile(readFile("defaults/preferences/adblockplus.js"));
+my @files = ();
+my $installManifest = fixupFile(readFile("install.rdf"));
+push @files, ["install.rdf", $installManifest];
 
+my $cleanManifest = $installManifest;
+$cleanManifest =~ s/<(\w+:)?targetApplication>.*?<\/\1targetApplication>//gs;
+$cleanManifest =~ s/<(\w+:)?requires>.*?<\/\1requires>//gs;
+
+die "Failed to extract extension name from install manifest" unless $cleanManifest =~ /<(\w+:)?name>([^<>]+)<\/\1name>/;
+my $name = $2;
+
+die "Failed to extract extension ID from install manifest" unless $cleanManifest =~ /<(\w+:)?id>([^<>]+)<\/\1id>/;
+my $id = $2;
+
+my $chromeManifest = fixupFile(readFile("chrome.manifest"));
 my $baseURL = cwd;
 $baseURL =~ s/\\/\//g;
 $baseURL = "file:///$baseURL/chrome";
-$manifest =~ s~jar:chrome/adblockplus\.jar!~$baseURL~g;
-$manifest =~ s~^content ~content   mochikit $baseURL/content/mochitest/\n$&~m;
+$chromeManifest =~ s~jar:chrome/\w+\.jar!~$baseURL~g;
+$chromeManifest =~ s~^content ~content   mochikit $baseURL/content/mochitest/\n$&~m if -d "chrome/content/mochitest";
+
+push @files, ["chrome.manifest", $chromeManifest];
+
+my @dirs;
+if (open(local *DIRS, ".profileDirs"))
+{
+  @dirs = map {s/[\r\n]//g;$_} <DIRS>;
+  close(DIRS);
+}
+unless (@dirs)
+{
+  print STDERR <<EOT;
+This script requires a file with the name .profileDirs to be in the current
+directory. Please create this file and specify the directories of the profiles
+where you want to install a test version of $name, one per line.
+For example:
+
+  c:\\Documents and Setting\\<user>\\Application Data\\Mozilla\\Firefox\\Profiles\\<seed>.default
+  c:\\Documents and Setting\\<user>\\Application Data\\Songbird1\\Profiles\\<seed>.default
+EOT
+  exit 1;
+}
+
+foreach my $file (<components/*.js>, <defaults/preferences/*.js>)
+{
+  push @files, [$file, fixupFile(readFile($file))];
+}
 
 foreach my $dir (@dirs)
 {
@@ -58,18 +73,26 @@ foreach my $dir (@dirs)
     next;
   }
 
-  my $baseDir = "$dir/extensions/{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}";
+  my $baseDir = "$dir/extensions/$id";
   rm_rec($baseDir);
 
   mkdir($baseDir);
-  mkdir("$baseDir/components");
-  mkdir("$baseDir/defaults");
-  mkdir("$baseDir/defaults/preferences");
 
-  writeFile("$baseDir/install.rdf", $install);
-  writeFile("$baseDir/chrome.manifest", $manifest);
-  writeFile("$baseDir/components/nsAdblockPlus.js", $component);
-  writeFile("$baseDir/defaults/preferences/adblockplus.js", $prefs);
+  foreach my $file (@files)
+  {
+    my ($filename, $content) = @$file;
+
+    my @parentDirs = split(/\//, $filename);
+    pop @parentDirs;
+    my $parentDir = $baseDir;
+    foreach (@parentDirs)
+    {
+      $parentDir .= "/" . $_;
+      mkdir($parentDir);
+    }
+
+    writeFile("$baseDir/$filename", $content);
+  }
 }
 
 sub readFile
