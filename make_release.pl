54f9874581e391cf93936c2ad66089b12dd95a4a
trev <none@none>
2008-10-23 08:14:06 +0200

    Excluding CVS metadata no longer necessary

--- make_release.pl
+++ make_release.pl
@@ -21,7 +21,7 @@ close(VERSION);
 do './create_xpi.pl';
 
 opendir(LOCALES, "chrome/locale");
-my @locales = grep {!/[^\w\-]/ && !/CVS/} readdir(LOCALES);
+my @locales = grep {!/[^\w\-]/} readdir(LOCALES);
 closedir(LOCALES);
 
 # Create new single-locale builds
