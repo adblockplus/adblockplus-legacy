9c644a6078d6331afaba30ff134b83753500af94
Wladimir Palant <trev@adblockplus.org>
2008-11-04 02:12:30 +0100

    Exclude incomplete locales from testing

--- test_locales.pl
+++ test_locales.pl
@@ -61,7 +61,7 @@ sub makeLocaleList
   my %locales = ();
   foreach my $dir (keys %paths) {
     opendir(local* DIR, $paths{$dir}) or die "Could not open directory $paths{$dir}";
-    my @locales = grep {!/[^\w\-]/} readdir(DIR);
+    my @locales = grep {!/[^\w\-]/ && !-e("$paths{$dir}/$_/.incomplete")} readdir(DIR);
     $locales{$_} = 1 foreach @locales;
     closedir(DIR);
   }
