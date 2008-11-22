d265053b27366b150e68e3721e8946281876aed1
Wladimir Palant <trev@adblockplus.org>
2008-11-22 13:43:59 +0100

    Adding Italian to the list of K-Meleon languages

--- kmeleon/build_all.pl
+++ kmeleon/build_all.pl
@@ -7,7 +7,7 @@ my $version = <VERSION>;
 $version =~ s/[^\w\.]//gs;
 close(VERSION);
 
-my @locales = qw(en-US de-DE ru-RU fr-FR es-ES pl-PL);
+my @locales = qw(en-US de-DE ru-RU fr-FR es-ES it-IT pl-PL);
 foreach my $locale (@locales) {
   system("$^X build.pl adblockplus-kmeleon-$version-$locale.zip $locale");
 }
