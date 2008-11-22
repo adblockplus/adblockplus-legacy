5da01c24df3114a8f87c8c00f108fe677de0e64a
Wladimir Palant <trev@adblockplus.org>
2008-11-22 13:43:59 +0100

    Adding Italian to the list of K-Meleon languages

--- kmeleon/build_devbuild.pl
+++ kmeleon/build_devbuild.pl
@@ -11,7 +11,7 @@ my ($sec, $min, $hour, $day, $mon, $year) = localtime;
 my $build = sprintf("%04i%02i%02i%02i", $year+1900, $mon+1, $day, $hour);
 my $locale = (@ARGV ? join("-", '', @ARGV) : "");
 
-@ARGV = qw(en-US de-DE ru-RU fr-FR es-ES pl-PL) unless @ARGV;
+@ARGV = qw(en-US de-DE ru-RU fr-FR es-ES it-IT pl-PL) unless @ARGV;
 
 system("$^X build.pl adblockplus-kmeleon-$version+.$build$locale.zip +.$build @ARGV");
 
--- kmeleon/build_devbuilds.pl
+++ kmeleon/build_devbuilds.pl
@@ -2,7 +2,7 @@
 
 use strict;
 
-my @locales = qw(en-US de-DE ru-RU fr-FR es-ES pl-PL);
+my @locales = qw(en-US de-DE ru-RU fr-FR es-ES it-IT pl-PL);
 foreach my $locale (@locales) {
   system("$^X build_devbuild.pl $locale");
 }
