3b416bda0a3ba4cb323e301a39fba7a564674ab2
Wladimir Palant <trev@adblockplus.org>
2008-12-03 21:59:17 +0100

    Make sure dev build version numbers always consist of three digit groups

--- make_devbuild.pl
+++ make_devbuild.pl
@@ -34,6 +34,10 @@ my $version = <VERSION>;
 $version =~ s/[^\w\.]//gs;
 close(VERSION);
 
+# Pad the version with zeroes to get version comparisons
+# right (1.2+ > 1.2.1 but 1.2.0+ < 1.2.1)
+$version .= ".0" while ($version =~ tr/././ < 2);
+
 my ($sec, $min, $hour, $day, $mon, $year) = localtime;
 my $build = sprintf("%04i%02i%02i%02i", $year+1900, $mon+1, $day, $hour);
 
