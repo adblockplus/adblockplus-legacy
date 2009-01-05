09c8d895a534394a25660022447e32b76dbe420c
Wladimir Palant <trev@adblockplus.org>
2009-01-05 08:39:07 +0100

    Don't call timeLine.log() in release builds

--- create_xpi.pl
+++ create_xpi.pl
@@ -25,6 +25,10 @@ if (@ARGV && $ARGV[0] =~ /^\+/)
   $params{devbuild} = $ARGV[0];
   shift @ARGV;
 }
+else
+{
+  $params{postprocess_line} = \&removeTimeLine;
+}
 
 $params{locales} = \@ARGV if @ARGV;
 
@@ -40,6 +44,15 @@ my @files = grep {-e $_} ('components', 'defaults', 'install.js', 'install.rdf',
 
 $pkg->makeXPI($xpiFile, "chrome/$baseName.jar", @files);
 unlink("chrome/$baseName.jar");
+
+sub removeTimeLine
+{
+  my ($file, $line) = @_;
+
+  return "\n" if $file =~ /\.js$/ && $line =~ /\btimeLine\.log\(/;
+
+  return $line;
+}
 
 sub readFile
 {
