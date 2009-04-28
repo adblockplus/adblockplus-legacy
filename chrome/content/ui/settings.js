bccbf2bb9024e2b316290076b831a32c03972b3d
Wladimir Palant <trev@adblockplus.org>
2009-04-28 12:45:22 +0200

    Topic 3850: Disabled filters shouldn't be marked as slow

--- chrome/content/ui/settings.js
+++ chrome/content/ui/settings.js
@@ -1215,7 +1215,7 @@ function showTreeTooltip(/**Event*/ event) /**Boolean*/
   treeView.boxObject.getCellAt(event.clientX, event.clientY, row, col, childElement);
 
   let [subscription, filter] = treeView.getRowInfo(row.value);
-  if (filter instanceof abp.RegExpFilter && !filter.shortcut && col.value && col.value.id == "col-slow")
+  if (row.value && col.value && col.value.id == "col-slow" && treeView.getCellText(row.value, col.value))
   {
     E("tree-tooltip").setAttribute("label", abp.getString("filter_regexp_tooltip"));
     return true;
@@ -1275,8 +1275,8 @@ function compareText(/**Filter*/ filter1, /**Filter*/ filter2)
  */
 function compareSlow(/**Filter*/ filter1, /**Filter*/ filter2)
 {
-  let isSlow1 = (filter1 instanceof abp.RegExpFilter && !filter1.shortcut ? 1 : 0);
-  let isSlow2 = (filter2 instanceof abp.RegExpFilter && !filter2.shortcut ? 1 : 0);
+  let isSlow1 = (filter1 instanceof abp.RegExpFilter && !filter1.disabled && !filter1.shortcut ? 1 : 0);
+  let isSlow2 = (filter2 instanceof abp.RegExpFilter && !filter2.disabled && !filter2.shortcut ? 1 : 0);
   return isSlow1 - isSlow2;
 }
 
@@ -1474,7 +1474,7 @@ let treeView = {
       if (col == "col-filter")
         return filter.text;
       else if (col == "col-slow")
-        return (filter instanceof abp.RegExpFilter && !filter.shortcut ? "!" : null);
+        return (filter instanceof abp.RegExpFilter && !filter.shortcut && !filter.disabled && !subscription.disabled ? "!" : null);
       else if (filter instanceof abp.ActiveFilter)
       {
         if (col == "col-hitcount")
