f1aa47ffa04ab0f50d6e592e017c5925c5c8ea1a
Wladimir Palant <trev@adblockplus.org>
2009-05-08 09:11:56 +0200

    Adding tests for the status displayed by ABP icon

---
+++ chrome/content/mochitest/tests/ui/common.js
@@ -0,0 +1,23 @@
+var geckoVersion = 0;
+try {
+  geckoVersion = Components.classes["@mozilla.org/xre/app-info;1"]
+                           .getService(Components.interfaces.nsIXULAppInfo)
+                           .platformVersion;
+} catch (e) {}
+
+function compareGeckoVersion(version)
+{
+  return Components.classes["@mozilla.org/xpcom/version-comparator;1"]
+                   .createInstance(Components.interfaces.nsIVersionComparator)
+                   .compare(geckoVersion, version);
+}
+
+function getBrowserWindow()
+{
+  return window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+               .getInterface(Components.interfaces.nsIWebNavigation)
+               .QueryInterface(Components.interfaces.nsIDocShellTreeItem)
+               .rootTreeItem
+               .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+               .getInterface(Components.interfaces.nsIDOMWindow);
+}
