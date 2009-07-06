6c5eb81975c2ae4f8069ecc259497dbb20316593
Wladimir Palant <trev@adblockplus.org>
2009-07-06 03:59:40 +0200

    Don't clear subscription wrappers when changes are applied, subscriptions (unlike filters) keep their wrappers

--- chrome/content/ui/settings.js
+++ chrome/content/ui/settings.js
@@ -2713,7 +2713,6 @@ let treeView = {
         subscriptions.push(subscription.__proto__);
       }
 
-      subscriptionWrappers = {__proto__: null};
       filterWrappers = {__proto__: null};
 
       for each (let subscription in filterStorage.subscriptions.slice())
