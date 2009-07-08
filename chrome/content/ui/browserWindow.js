df293a8708568ed29c6b66ae2836218f23b2a3e8
Wladimir Palant <trev@adblockplus.org>
2009-07-08 11:54:42 +0200

    Insert toolbar icon in SeaMonkey on upgrade from pre-1.1 versions
    --HG--
    extra : rebase_source : 12ba50f177b02d01374b06628c708f20d707ea03

--- chrome/content/ui/browserWindow.js
+++ chrome/content/ui/browserWindow.js
@@ -206,16 +206,25 @@ function abpInit() {
   }
 
   // First run actions
-  if (!("doneFirstRunActions" in prefs) && abp.versionComparator.compare(prefs.lastVersion, "0.0") <= 0)
+  if (!("doneFirstRunActions" in prefs))
   {
     // Don't repeat first run actions if new window is opened
     prefs.doneFirstRunActions = true;
 
-    // Add ABP icon to toolbar if necessary
-    abp.runAsync(abpInstallInToolbar);
+    if (abp.versionComparator.compare(prefs.lastVersion, "0.0") <= 0)
+    {
+      // Add ABP icon to toolbar if necessary
+      abp.runAsync(abpInstallInToolbar);
 
-    // Show subscriptions dialog if the user doesn't have any subscriptions yet
-    abp.runAsync(abpShowSubscriptions);
+      // Show subscriptions dialog if the user doesn't have any subscriptions yet
+      abp.runAsync(abpShowSubscriptions);
+    }
+    else if (abp.versionComparator.compare(prefs.lastVersion, "1.1") < 0 &&
+             Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULAppInfo).ID == "{92650c4d-4b8e-4d2a-b7eb-24ecf4f6b63a}")
+    {
+      // Before version 1.1 we didn't add toolbar icon in SeaMonkey, do it now
+      abp.runAsync(abpInstallInToolbar);
+    }
   }
 
   abp.runAsync(abpInitImageManagerHiding);
