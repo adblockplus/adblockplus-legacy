b8ed8e353f9411a967af904728901c50f47c160c
Wladimir Palant <trev@adblockplus.org>
2009-08-28 15:14:25 +0200

    Reorganized build scripts: Moved the real scripts out of the extension directories and replaced by dummy scripts. This gets rid of multiple copies of the same scripts and makes sure new development builds are not created just because release scripts changed.
    --HG--
    rename : create_xpi.pl => make_devbuild.pl
    rename : create_xpi.pl => setupTestEnvironment.pl

--- create_xpi.pl
+++ create_xpi.pl
@@ -1,68 +1,7 @@
 #!/usr/bin/perl
 
-#############################################################################
-# This script will create an extension build. Usually, this script          #
-# shouldn't be run directly, use make_devbuild.pl instead.                  #
-#############################################################################
+# This is a dummy, its purpose is to call a script with the same name in the parent directory
 
-use strict;
-use warnings;
-use lib qw(..);
-use Packager;
-
-my $manifest = readFile("chrome.manifest");
-unless ($manifest =~ /\bjar:chrome\/(\S+?)\.jar\b/)
-{
-  die "Could not find JAR file name in chrome.manifest";
-}
-my $baseName = $1;
-
-my %params = ();
-
-my $xpiFile = shift @ARGV || "$baseName.xpi";
-if (@ARGV && $ARGV[0] =~ /^\+/)
-{
-  $params{devbuild} = $ARGV[0];
-  shift @ARGV;
-}
-else
-{
-  $params{postprocess_line} = \&removeTimeLine;
-}
-
-$params{locales} = \@ARGV if @ARGV;
-
-my $pkg = Packager->new(\%params);
-$pkg->readVersion('version');
-$pkg->readLocales('chrome/locale') unless exists $params{locales};
-
-chdir('chrome');
-$pkg->makeJAR("$baseName.jar", 'content', 'skin', 'locale', '-/tests', '-/mochitest', '-/.incomplete');
-chdir('..');
-
-my @files = grep {-e $_} ('components', 'defaults', 'install.rdf', 'chrome.manifest', 'icon.png');
-
-$pkg->makeXPI($xpiFile, "chrome/$baseName.jar", @files);
-unlink("chrome/$baseName.jar");
-
-sub removeTimeLine
-{
-  my ($file, $line) = @_;
-
-  return "\n" if $file =~ /\.js$/ && $line =~ /\btimeLine\.(\w+)\(/;
-
-  return $line;
-}
-
-sub readFile
-{
-  my $file = shift;
-
-  open(local *FILE, "<", $file) || die "Could not read file '$file'";
-  binmode(FILE);
-  local $/;
-  my $result = <FILE>;
-  close(FILE);
-
-  return $result;
-}
+$0 =~ s/(.*[\\\/])//g;
+chdir($1) if $1;
+do "../$0";
--- make_devbuild.pl
+++ make_devbuild.pl
@@ -1,60 +1,7 @@
 #!/usr/bin/perl
 
-#############################################################################
-# This script will create a development build of the extension. Without any #
-# command line arguments it will include all available locales in the       #
-# development build, command line arguments are interpreted as a list of    #
-# locales to be included.                                                   #
-#                                                                           #
-# Creating a development build with all locales:                            #
-#                                                                           #
-#   perl make_devbuild.pl                                                   #
-#                                                                           #
-# Creating a development build with en-US locale only:                      #
-#                                                                           #
-#   perl make_devbuild.pl en-US                                             #
-#                                                                           #
-# Creating a development build with English, German and Russian locales:    #
-#                                                                           #
-#   perl make_devbuild.pl en-US de-DE ru-RU                                 #
-#                                                                           #
-#############################################################################
+# This is a dummy, its purpose is to call a script with the same name in the parent directory
 
-use strict;
-
-my $manifest = readFile("chrome.manifest");
-unless ($manifest =~ /\bjar:chrome\/(\S+?)\.jar\b/)
-{
-  die "Could not find JAR file name in chrome.manifest";
-}
-my $baseName = $1;
-
-open(VERSION, "version");
-my $version = <VERSION>;
-$version =~ s/[^\w\.]//gs;
-close(VERSION);
-
-# Pad the version with zeroes to get version comparisons
-# right (1.2+ > 1.2.1 but 1.2.0+ < 1.2.1)
-$version .= ".0" while ($version =~ tr/././ < 2);
-
-my ($sec, $min, $hour, $day, $mon, $year) = localtime;
-my $build = sprintf("%04i%02i%02i%02i", $year+1900, $mon+1, $day, $hour);
-
-my $locale = (@ARGV ? "-" . join("-", @ARGV) : "");
-@ARGV = ("$baseName-$version+.$build$locale.xpi", "+.$build", @ARGV);
-do './create_xpi.pl';
-die $@ if $@;
-
-sub readFile
-{
-  my $file = shift;
-
-  open(local *FILE, "<", $file) || die "Could not read file '$file'";
-  binmode(FILE);
-  local $/;
-  my $result = <FILE>;
-  close(FILE);
-
-  return $result;
-}
+$0 =~ s/(.*[\\\/])//g;
+chdir($1) if $1;
+do "../$0";
