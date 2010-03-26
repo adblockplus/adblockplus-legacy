64e501864c5a801f11a38dc58504222e2f97525b
Wladimir Palant <trev@adblockplus.org>
2010-03-26 11:12:37 +0100

    About dialog: Reworked list of contributors, most entries are now populated from available sources rather than being hardcoded

--- chrome/content/ui/about.js
+++ chrome/content/ui/about.js
@@ -22,7 +22,101 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-function fillInVersion()
+try
+{
+  Cu.import("resource://gre/modules/AddonManager.jsm");
+}
+catch (e) {}
+
+const addonID = "{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}";
+
+function init()
 {
   E("version").value = abp.getInstalledVersion();
+
+  if (typeof AddonManager != "undefined")
+  {
+    let addon = AddonManager.getAddon(addonId, function(addon)
+    {
+      setContributors(addon.contributors, addon.translators);
+    });
+  }
+  else
+  {
+    let ds = Cc["@mozilla.org/extensions/manager;1"].getService(Ci.nsIExtensionManager).datasource;
+    let rdf = Cc["@mozilla.org/rdf/rdf-service;1"].getService(Ci.nsIRDFService);
+    let root = rdf.GetResource("urn:mozilla:item:" + addonID);
+    
+    function emResource(prop)
+    {
+      return rdf.GetResource("http://www.mozilla.org/2004/em-rdf#" + prop);
+    }
+
+    function getTargets(prop)
+    {
+      let targets = ds.GetTargets(root, emResource(prop), true);
+      let result = [];
+      while (targets.hasMoreElements())
+        result.push(targets.getNext().QueryInterface(Ci.nsIRDFLiteral).Value);
+      return result;
+    }
+
+    setContributors(getTargets("contributor"), getTargets("translator"));
+  }
+}
+
+function cmpNoCase(a, b)
+{
+  let aLC = a.toLowerCase();
+  let bLC = b.toLowerCase();
+  if (aLC < bLC)
+    return -1;
+  else if (aLC > bLC)
+    return 1;
+  else
+    return 0;
+}
+
+function setContributors(contributors, translators)
+{
+  contributors.sort(cmpNoCase);
+  translators.sort(cmpNoCase);
+
+  E("developers").textContent = contributors.join(", ");
+  E("translators").textContent = translators.join(", ");
+
+  let request = new XMLHttpRequest();
+  request.open("GET", "chrome://adblockplus/content/ui/subscriptions.xml");
+  request.onload = setSubscriptionAuthors;
+  request.send(null);
+}
+
+function setSubscriptionAuthors()
+{
+  let doc = this.responseXML;
+  if (!doc || doc.documentElement.localName != "subscriptions")
+    return;
+
+  let authors = {__proto__: null};
+  for (let node = doc.documentElement.firstChild; node; node = node.nextSibling)
+  {
+    if (node.localName != "subscription" || !node.hasAttribute("author"))
+      continue;
+
+    for each (let author in node.getAttribute("author").split(","))
+    {
+      author = author.replace(/^\s+/, "").replace(/\s+$/, "");
+      if (author == "")
+        continue;
+
+      authors[author] = true;
+    }
+  }
+
+  let list = [];
+  for (let author in authors)
+    list.push(author);
+
+  list.sort(cmpNoCase)
+  E("subscriptionAuthors").textContent = list.join(", ");
 }
