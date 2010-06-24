e42046497733aa58fc5092c12dc43d9f4bddab2d
Wladimir Palant <trev@adblockplus.org>
2010-06-24 15:58:20 +0200

    K-Meleon: Added minimal UI integration, only opening the preference window seems to work properly for now

--- kmeleon/callbacks.cpp
+++ kmeleon/callbacks.cpp
@@ -28,12 +28,26 @@ WNDPROC origWndProc = NULL;
 WNDPROC origDialogWndProc = NULL;
 HHOOK hook = NULL;
 
-BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
   return TRUE;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-  if (message == WM_COMMAND) {
+JSBool InitCommandArgs(JSContext* cx, JSObject* globalObj, jsval* args, void* data)
+{
+  char* commandName = reinterpret_cast<char*>(data);
+  JSString* str = JS_NewStringCopyZ(cx, commandName);
+  if (!str)
+    return JS_FALSE;
+
+  args[0] = STRING_TO_JSVAL(str);
+  return JS_TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+  if (message == WM_COMMAND)
+  {
     WORD command = LOWORD(wParam) - cmdBase;
     char* commandName;
     PRBool done = PR_TRUE;
@@ -68,54 +82,40 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
         break;
     }
 
-    abpJSContextHolder holder;
-    JSObject* overlay = UnwrapJSObject(fakeBrowserWindow);
-    JSContext* cx = holder.get();
-    if (cx != nsnull && overlay != nsnull) {
-      JSString* str = JS_NewStringCopyZ(cx, commandName);
-      if (str != nsnull) {
-        jsval retval;
-        jsval args[] = {STRING_TO_JSVAL(str), INT_TO_JSVAL(hWnd), INT_TO_JSVAL(LOWORD(wParam))};
-        JS_CallFunctionName(cx, overlay, "onCommand", 3, args, &retval);
-      }
-    }
+    jsval args[] = {JSVAL_FALSE, INT_TO_JSVAL(hWnd), INT_TO_JSVAL(LOWORD(wParam))};
+    CallModuleMethod("onCommand", 3, args, nsnull, InitCommandArgs, reinterpret_cast<char*>(commandName));
+
     if (done)
       return TRUE;
   }
   else if (((message == TB_MBUTTONDOWN || message == TB_MBUTTONDBLCLK) && wParam == cmdBase + CMD_TOOLBAR) ||
-           ((message == SB_MBUTTONDOWN || message == SB_MBUTTONDBLCLK) && wParam == cmdBase + CMD_STATUSBAR)) {
-    abpJSContextHolder holder;
-    char param[] = "enable";
-    JSObject* overlay = UnwrapJSObject(fakeBrowserWindow);
-    JSContext* cx = holder.get();
-    if (cx != nsnull && overlay != nsnull) {
-      JSString* str = JS_NewString(cx, param, strlen(param));
-      if (str != nsnull) {
-        jsval arg = STRING_TO_JSVAL(str);
-        jsval retval;
-        JS_CallFunctionName(cx, overlay, "onCommand", 1, &arg, &retval);
-      }
-    }
+           ((message == SB_MBUTTONDOWN || message == SB_MBUTTONDBLCLK) && wParam == cmdBase + CMD_STATUSBAR))
+  {
+    char commandName[] = "enable";
+    jsval arg;
+    CallModuleMethod("onCommand", 1, &arg, nsnull, InitCommandArgs, reinterpret_cast<char*>(commandName));
   }
   else if ((message == TB_RBUTTONDOWN && wParam == cmdBase + CMD_TOOLBAR) ||
-           (message == SB_RBUTTONDOWN && wParam == cmdBase + CMD_STATUSBAR)) {
+           (message == SB_RBUTTONDOWN && wParam == cmdBase + CMD_STATUSBAR))
+  {
     ShowContextMenu(hWnd, message != TB_RBUTTONDOWN);
   }
-  else if (message == WM_NOTIFY) {
+  else if (message == WM_NOTIFY)
+  {
     LPNMHDR notifyHeader = (LPNMHDR) lParam;
     if ((notifyHeader->code == (UINT)TTN_NEEDTEXTA || notifyHeader->code == (UINT)TTN_NEEDTEXTW) &&
-        (wParam == cmdBase + CMD_TOOLBAR || wParam == cmdBase + CMD_STATUSBAR)) {
-      abpJSContextHolder holder;
-      JSObject* overlay = UnwrapJSObject(fakeBrowserWindow);
-      JSContext* cx = holder.get();
-      if (cx != nsnull && overlay != nsnull) {
-        jsval args[] = {
-            wParam == cmdBase + CMD_STATUSBAR ? JSVAL_TRUE : JSVAL_FALSE,
-            notifyHeader->code == (UINT)TTN_NEEDTEXTW ? JSVAL_TRUE : JSVAL_FALSE
-        };
-        jsval retval;
-        if (JS_CallFunctionName(cx, overlay, "getTooltipText", 2, args, &retval)) {
-          JSString* text = JS_ValueToString(cx, retval);
+        (wParam == cmdBase + CMD_TOOLBAR || wParam == cmdBase + CMD_STATUSBAR))
+    {
+      jsval args[] = {
+          wParam == cmdBase + CMD_STATUSBAR ? JSVAL_TRUE : JSVAL_FALSE,
+          notifyHeader->code == (UINT)TTN_NEEDTEXTW ? JSVAL_TRUE : JSVAL_FALSE
+      };
+      jsval retval;
+      if (CallModuleMethod("getTooltipText", 2, args, &retval))
+      {
+        if (JSVAL_IS_STRING(retval))
+        {
+          JSString* text = JSVAL_TO_STRING(retval);
           if (notifyHeader->code == (UINT)TTN_NEEDTEXTA)
           {
               LPTOOLTIPTEXTA lpTiptext = (LPTOOLTIPTEXTA) lParam;
@@ -126,8 +126,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
               LPTOOLTIPTEXTW lpTiptext = (LPTOOLTIPTEXTW) lParam;
               lpTiptext->lpszText = (LPWSTR)JS_GetStringChars(text);
           }
-          return 0;
         }
+        return 0;
       }
     }
   }
@@ -138,7 +138,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
     return CallWindowProcA(origWndProc, hWnd, message, wParam, lParam);
 }
 
-LRESULT CALLBACK DialogWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK DialogWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
   LRESULT retVal;
   if (IsWindowUnicode(hWnd))
     retVal = CallWindowProcW(origDialogWndProc, hWnd, message, wParam, lParam);
@@ -146,7 +147,8 @@ LRESULT CALLBACK DialogWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lP
     retVal = CallWindowProcA(origDialogWndProc, hWnd, message, wParam, lParam);
 
   char* eventHandler;
-  switch (message) {
+  switch (message)
+  {
     case WM_SIZE:
       eventHandler = "onDialogResize";
       break;
@@ -158,24 +160,22 @@ LRESULT CALLBACK DialogWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lP
       break;
   }
 
-  if (eventHandler) {
-    abpJSContextHolder holder;
-    JSObject* overlay = UnwrapJSObject(fakeBrowserWindow);
-    if (holder.get() && overlay) {
-      jsval retval;
-      jsval arg = INT_TO_JSVAL((int32)hWnd);
-
-      JS_CallFunctionName(holder.get(), overlay, eventHandler, 1, &arg, &retval);
-    }
+  if (eventHandler)
+  {
+    jsval arg = INT_TO_JSVAL((int32)hWnd);
+    CallModuleMethod(eventHandler, 1, &arg);
   }
 
   return retVal;
 }
 
-LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam) {
-  if (nCode == HC_ACTION) {
+LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+  if (nCode == HC_ACTION)
+  {
     CWPRETSTRUCT* params = (CWPRETSTRUCT*)lParam;
-    if (params->message == WM_DRAWITEM) {
+    if (params->message == WM_DRAWITEM)
+    {
       DRAWITEMSTRUCT* dis = (DRAWITEMSTRUCT*)params->lParam;
       WORD id = dis->itemID - cmdBase;
       if (dis->CtlType == ODT_MENU && id < NUM_COMMANDS)
