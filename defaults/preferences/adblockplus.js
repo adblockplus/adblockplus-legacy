b4b9c2f1080f11301d64c43cc94482d5ef762e21
Wladimir Palant <trev@adblockplus.org>
2010-09-29 13:39:55 +0200

    Issue reporter: Show recently submitted reports on first page

--- defaults/preferences/adblockplus.js
+++ defaults/preferences/adblockplus.js
@@ -29,3 +29,4 @@ pref("extensions.adblockplus.savestats", true);
 pref("extensions.adblockplus.composer_default", 2);
 pref("extensions.adblockplus.clearStatsOnHistoryPurge", true);
 pref("extensions.adblockplus.report_submiturl", "https://reports.adblockplus.org/submitReport?version=1&guid=%GUID%&lang=%LANG%");
+pref("extensions.adblockplus.recentReports", "[]");
