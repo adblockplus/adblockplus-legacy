d24f425ea08a5b80ac9685f5c0d8821ce0d52922
Wladimir Palant <trev@adblockplus.org>
2010-10-14 15:40:32 +0200

    Blockable items: made sure scrolling to node works correctly again, particularly in latest Firefox 4 nightly
    --HG--
    branch : experimental

--- chrome/content/ui/flasher.js
+++ chrome/content/ui/flasher.js
@@ -31,27 +31,37 @@ var flasher = {
   count: 0,
   timer: null,
 
-  flash: function(nodes) {
+  flash: function(nodes)
+  {
     this.stop();
+    if (nodes)
+      nodes = nodes.filter(function(node) node.nodeType == Node.ELEMENT_NODE);
     if (!nodes || !nodes.length)
       return;
 
-    if (Prefs.flash_scrolltoitem && ("document" in nodes[0] || nodes[0].ownerDocument)) {
+    if (Prefs.flash_scrolltoitem && nodes[0].ownerDocument)
+    {
       // Ensure that at least one node is visible when flashing
-      var wnd = ("document" in nodes[0] ? nodes[0] : nodes[0].ownerDocument.defaultView);
-      try {
-        var viewer = wnd.QueryInterface(Ci.nsIInterfaceRequestor)
-                        .getInterface(Ci.nsIWebNavigation)
-                        .QueryInterface(Ci.nsIDocShellTreeItem)
-                        .rootTreeItem
-                        .QueryInterface(Ci.nsIInterfaceRequestor)
-                        .getInterface(Ci.nsIDOMWindow)
-                        .document.getElementById("abp-hooks")
-                        .wrappedJSObject
-                        .getBrowser()
-                        .markupDocumentViewer;
+      let wnd = nodes[0].ownerDocument.defaultView;
+      try
+      {
+        let hooks = wnd.QueryInterface(Ci.nsIInterfaceRequestor)
+                       .getInterface(Ci.nsIWebNavigation)
+                       .QueryInterface(Ci.nsIDocShellTreeItem)
+                       .rootTreeItem
+                       .QueryInterface(Ci.nsIInterfaceRequestor)
+                       .getInterface(Ci.nsIDOMWindow)
+                       .document.getElementById("abp-hooks");
+        if (hooks.wrappedJSObject)
+          hooks = hooks.wrappedJSObject;
+                        
+        let viewer = hooks.getBrowser().markupDocumentViewer;
         viewer.scrollToNode(nodes[0]);
-      } catch(e) {}
+      }
+      catch(e)
+      {
+        Cu.reportError(e);
+      }
     }
 
     this.nodes = nodes;
