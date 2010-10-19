43f78dfbce481cc67ffa0fbbb47921f02253b5b6
Wladimir Palant <trev@adblockplus.org>
2010-10-19 16:19:47 +0200

    Moved extension metadata to a central location. install.rdf is now being generated automatically and setting homepage is no longer up to translators.
    --HG--
    branch : experimental

--- make_release.pl
+++ make_release.pl
@@ -2,8 +2,6 @@
 
 # This is a dummy, its purpose is to call a script with the same name in the buildtools repository
 
-our $BRANCH_NAME = "ADBLOCK_PLUS";
-
 $0 =~ s/(.*[\\\/])//g;
 chdir($1) if $1;
 
