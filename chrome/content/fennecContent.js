f299a978a1c5fbeac779b86cca57a3172061e3a7
Wladimir Palant <trev@adblockplus.org>
2011-03-17 11:27:31 +0100

    Fennec 4.0: Fixed handling of abp: links

--- chrome/content/fennecContent.js
+++ chrome/content/fennecContent.js
@@ -25,3 +25,24 @@
 
 if (!("@adblockplus.org/abp/policy;1" in Components.classes))
   Components.utils.import("chrome://adblockplus-modules/content/ContentPolicyRemote.jsm");
+
+addEventListener("click", function(event)
+{
+  // Ignore right-clicks
+  if (event.button == 2)
+    return;
+
+  // Search the link associated with the click
+  let link = event.target;
+  while (link && !(link instanceof Ci.nsIDOMHTMLAnchorElement))
+    link = link.parentNode;
+
+  if (!link || !/^abp:\/*subscribe\/*\?(.*)/i.test(link.href))  /**/
+    return;
+
+  // This is our link - make sure the browser doesn't handle it
+  event.preventDefault();
+  event.stopPropagation();
+
+  sendAsyncMessage("AdblockPlus:LinkClick", link.href);
+}, true);
