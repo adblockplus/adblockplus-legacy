b269236a439277b7b75f7ca99a9b5e52d9f512a5
Wladimir Palant <trev@adblockplus.org>
2011-06-01 16:19:15 +0200

    Topic 6035 - First steps to implement filter preferences, subscriptions tab
    --HG--
    branch : experimental

---
+++ chrome/content/ui/filters.js
@@ -0,0 +1,141 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Adblock Plus.
+ *
+ * The Initial Developer of the Original Code is
+ * Wladimir Palant.
+ * Portions created by the Initial Developer are Copyright (C) 2006-2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+/**
+ * Initialization function, called when the window is loaded.
+ */
+function init()
+{
+  // Install listener
+  FilterStorage.addObserver(onChange);
+
+  reloadSubscriptions();
+}
+
+function shutdown()
+{
+  // Remove listener
+  FilterStorage.removeObserver(onChange);
+}
+
+function reloadSubscriptions()
+{
+  // Remove existing entries if any
+  let remove = [];
+  for (let child = E("subscriptions").firstChild; child; child = child.nextSibling)
+    if (!child.id)
+      remove.push(child);
+  remove.map(function(child) child.parentNode.removeChild(child));
+
+  // Now add all subscriptions
+  let subscriptions = FilterStorage.subscriptions.filter(function(subscription) !(subscription instanceof SpecialSubscription));
+  if (subscriptions.length)
+  {
+    for each (let subscription in subscriptions)
+      addSubscription(subscription, null);
+
+    // Set the focus to the subscriptions list by default
+    let listElement = E("subscriptions");
+    listElement.focus();
+
+    // Make sure first list item is selected after list initialization
+    Utils.runAsync(function()
+    {
+      listElement.selectItem(listElement.getItemAtIndex(listElement.getIndexOfFirstVisibleRow()));
+    });
+  }
+  E("noSubscriptions").hidden = subscriptions.length;
+}
+
+function addSubscription(/**Subscription*/ subscription, /**Node*/ insertBefore) /**Node*/
+{
+  subscription.downloadInProgress = Synchronizer.isExecuting(subscription.url)
+  let node = processTemplate(E("subscriptionTemplate"), subscription);
+  delete subscription.downloadInProgress;
+
+  if (insertBefore)
+    E("subscriptions").insertBefore(node, insertBefore);
+  else
+    E("subscriptions").appendChild(node);
+  return node;
+}
+
+function processTemplate(/**Node*/ template, /**Object*/ data) /**Node*/
+{
+  // Use a sandbox to resolve attributes (for convenience, not security)
+  let sandbox = Cu.Sandbox(window);
+  sandbox.obj = data;
+  sandbox.formatTime = Utils.formatTime;
+
+  // Clone template but remove id/hidden attributes from it
+  let result = template.cloneNode(true);
+  result.removeAttribute("id");
+  result.removeAttribute("hidden");
+
+  // Resolve any attributes of the for attr="{obj.foo}"
+  let conditionals = [];
+  let nodeIterator = document.createNodeIterator(result, NodeFilter.SHOW_ELEMENT, null, false);
+  for (let node = nodeIterator.nextNode(); node; node = nodeIterator.nextNode())
+  {
+    if (node.localName == "if")
+      conditionals.push(node);
+    for (let i = 0; i < node.attributes.length; i++)
+    {
+      let attribute = node.attributes[i];
+      let len = attribute.value.length;
+      if (len >= 2 && attribute.value[0] == "{" && attribute.value[len - 1] == "}")
+        attribute.value = Cu.evalInSandbox(attribute.value.substr(1, len - 2), sandbox);
+    }
+  }
+
+  // Process <if> tags - remove if condition is false, replace by their children
+  // if it is true
+  for each (let node in conditionals)
+  {
+    let fragment = document.createDocumentFragment();
+    let condition = node.getAttribute("condition");
+    if (condition == "false")
+      condition = false;
+    for (let i = 0; i < node.childNodes.length; i++)
+    {
+      let child = node.childNodes[i];
+      if (child.localName == "elif" || child.localName == "else")
+      {
+        if (condition)
+          break;
+        condition = (child.localName == "elif" ? child.getAttribute("condition") : true);
+        if (condition == "false")
+          condition = false;
+      }
+      else if (condition)
+        fragment.appendChild(node.childNodes[i--]);
+    }
+    node.parentNode.replaceChild(fragment, node);
+  }
+  return result;
+}
+
+function onChange()
+{
+}
