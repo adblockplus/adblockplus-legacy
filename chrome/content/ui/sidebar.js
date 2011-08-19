c68bd45ea252473be2c1bee29000325b3b427520
Wladimir Palant <trev@adblockplus.org>
2011-08-19 15:25:30 +0200

    Bug 679675 - Blockable items could delay garbage collection of a compartment, detaching tree view explicitly now
    --HG--
    branch : experimental

--- chrome/content/ui/sidebar.js
+++ chrome/content/ui/sidebar.js
@@ -116,6 +116,7 @@ function cleanUp() {
   requestNotifier.shutdown();
   FilterNotifier.removeListener(reloadDisabledFilters);
   Prefs.removeListener(onPrefChange);
+  E("list").view = null;
 
   abpHooks.getBrowser().removeProgressListener(progressListener);
   mainWin.removeEventListener("unload", mainUnload, false);
