959e988edc36510dabd920ee75a22e7458b14636
Wladimir Palant <trev@adblockplus.org>
2012-01-04 09:37:37 +0100

    Relicensed from MPL 1.1 to MPL 2.0

--- normalizeLocales.pl
+++ normalizeLocales.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/perl
 
+# This Source Code is subject to the terms of the Mozilla Public License
+# version 2.0 (the "License"). You can obtain a copy of the License at
+# http://mozilla.org/MPL/2.0/.
+
 # This script will adjust the locales as received from Babelzilla - normalize
 # newlines and remove comments that have been pointlessly copied over from
 # en-US.
--- test_locales.pl
+++ test_locales.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/perl
 
+# This Source Code is subject to the terms of the Mozilla Public License
+# version 2.0 (the "License"). You can obtain a copy of the License at
+# http://mozilla.org/MPL/2.0/.
+
 use strict;
 use warnings;
 use lib qw(buildtools);
