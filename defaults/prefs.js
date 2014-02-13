1305e5dfc19fcef6e397f08570060c75a834ba40
Thomas Greiner <thomas@adblockplus.org>
2014-02-13 14:12:48 +0100

    Implemented anti-adblock message notification

--- defaults/prefs.js
+++ defaults/prefs.js
@@ -34,3 +34,4 @@ pref("extensions.adblockplus.hideContributeButton", false);
 pref("extensions.adblockplus.blockableItemsSize", {width: 200, height: 200});
 pref("extensions.adblockplus.notificationurl", "https://notification.adblockplus.org/notification.json");
 pref("extensions.adblockplus.notificationdata", {});
+pref("extensions.adblockplus.subscriptions_antiadblockurl", "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt");
